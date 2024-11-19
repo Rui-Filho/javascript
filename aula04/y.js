27d9235e373950198c5e0a0d4ad6df4a4fb8a275
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-11-19 08:47:29 -0300

    res

---
+++ aula04/y.js
@@ -0,0 +1,10 @@
+let g = false
+let h = undefined
+
+function f(ser){
+    g = true
+    h = ser
+}
+f("Gaby")
+console.log(g)
+console.log(h)
---
+++ aula04/y2.js
@@ -0,0 +1,9 @@
+
+let res = 10
+for(let con = 1;con < 4;con++){
+    res=res*10
+     
+}
+
+console.log(res)
+
