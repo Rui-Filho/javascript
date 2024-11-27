0e21b27794f7c368e105fe2bcb1df1626c797315
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-11-27 11:35:56 -0300

    res

--- exe final/script.js
+++ exe final/script.js
@@ -1,61 +1,37 @@
-
-let num = document.getElementById('fnum')
-let lista = document.getElementById('flista')
-let res = document.getElementById('res')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
 
-
 function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
+    if(Number(n) >= 1 && Number(n) <= 100) {
         return true
-    } else {
+    } else{
         return false
     }
-
+    
 }
 
 function inLista(n,l){
-    if(l.indexOf(Number(n)) != -1){
+    if(l.indexOf(Number(n)) != -1) {
         return true
     } else{
         return false
     }
 
+    
 }
 
-function adicionar (){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-        
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
-        res.innerHTML=''
-    }  else {
-        window.alert('Valor inválido ou já encontrado na lista')
-    }
-    num.value = ''
-    num.focus()
+function adicionar(){
+    if(isNumero(num.value)&& !inLista(num.value, valores)) {
+        window.alert('ok')
 
-}
-
-function finalizar(){
-    if(valores.length == 0){
-        alert('Adicione valores antes de finalizar.')
     } else {
+        window.alert('Valor inválido ou se encontra na lista')
 
-        let tot = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = 0
-        let media = 0
-        for(let pos in valores){
-            soma += valores[pos]
-            if(valores[pos] > maior)
-                maior= valores[pos]
-            if(valores[pos] < menor)
-                menor=valores[pos]
+    }
 
 
+}
 
-çççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççççç
+
